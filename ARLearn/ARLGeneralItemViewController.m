56a54be5fb69c58cb39e988642bafd5aa3a8f0dd
wvdvegt <veg@MacBookPro.local>
2015-05-27 13:32:26 +0200

    1) Cleaned code.

--- ARLearn/ARLGeneralItemViewController.m
+++ ARLearn/ARLGeneralItemViewController.m
@@ -329,110 +329,6 @@ CGFloat wbheight = 0.1f;
     [self.navigationController popViewControllerAnimated:YES];
 }
 
-#warning TODO is to visualize the tasks to do
-
-// See weSPOT PIM
-//
-//openQuestion =             {
-//    textDescription = "";
-//    type = "org.celstec.arlearn2.beans.generalItem.OpenQuestion";
-//    valueDescription = "";
-//    withAudio = 1;
-//    withPicture = 1;
-//    withText = 0;
-//    withValue = 0;
-//    withVideo = 0;
-//}
-
-//{
-//    answers =             (
-//                           {
-//                               answer = boring;
-//                               id = Xd07MTYNbBwQioy;
-//                               isCorrect = 0;
-//                               type = "org.celstec.arlearn2.beans.generalItem.MultipleChoiceAnswerItem";
-//                           },
-//                           {
-//                               answer = "not so boring...";
-//                               id = 3xrHNMQECVa81yh;
-//                               isCorrect = 0;
-//                               type = "org.celstec.arlearn2.beans.generalItem.MultipleChoiceAnswerItem";
-//                           },
-//                           {
-//                               answer = interesting;
-//                               id = 3nPYWc2gzqQvWlJ;
-//                               isCorrect = 0;
-//                               type = "org.celstec.arlearn2.beans.generalItem.MultipleChoiceAnswerItem";
-//                           }
-//                           );
-//    autoLaunch = 0;
-//    deleted = 0;
-//    description = "Tell me what you think of this lecture so far";
-//    fileReferences =             (
-//                                  {
-//                                      fileReference = "http://streetlearn.appspot.com/game/13876002/generalItems/13946007/audio";
-//                                      key = audio;
-//                                      type = "org.celstec.arlearn2.beans.generalItem.FileReference";
-//                                  }
-//                                  );
-//    gameId = 13876002;
-//    id = 13946007;
-//    lastModificationDate = 1417180691530;
-//    name = "This lecture is...";
-//    richText = "Tell me what you think of this lecture so far";
-//    roles =             (
-//    );
-//    scope = user;
-//    showCountDown = 0;
-//    sortKey = 0;
-//    type = "org.celstec.arlearn2.beans.generalItem.SingleChoiceTest";
-//}
-
-//{
-//    answers =             (
-//                           {
-//                               answer = "Read from your medical notes as you have written them?";
-//                               id = tNP9MpMeCe7QR5Y;
-//                               isCorrect = 0;
-//                               type = "org.celstec.arlearn2.beans.generalItem.MultipleChoiceAnswerItem";
-//                           },
-//                           {
-//                               answer = "Ask your receptionist to make the phone call?";
-//                               id = 5ogsXvHvJ8kfJmi;
-//                               isCorrect = 0;
-//                               type = "org.celstec.arlearn2.beans.generalItem.MultipleChoiceAnswerItem";
-//                           },
-//                           {
-//                               answer = "Ask your patient to make the phone call?";
-//                               id = lQKCWClD3pnx2D7;
-//                               isCorrect = 0;
-//                               type = "org.celstec.arlearn2.beans.generalItem.MultipleChoiceAnswerItem";
-//                           },
-//                           {
-//                               answer = "Use ISBAR for structured communication?";
-//                               id = cFTT2ABGaRQsAt7;
-//                               isCorrect = 0;
-//                               type = "org.celstec.arlearn2.beans.generalItem.MultipleChoiceAnswerItem";
-//                           }
-//                           );
-//    autoLaunch = 0;
-//    deleted = 0;
-//    description = "Which is the optimal way to ...";
-//    fileReferences =             (
-//    );
-//    gameId = 13876002;
-//    id = 5322433481408512;
-//    lastModificationDate = 1418120145353;
-//    name = "Multi-Select";
-//    richText = "Which is the optimal way to ...";
-//    roles =             (
-//    );
-//    scope = user;
-//    showCountDown = 0;
-//    sortKey = 0;
-//    type = "org.celstec.arlearn2.beans.generalItem.MultipleChoiceTest";
-//}
-
 #pragma mark - Properties
 
 -(NSString *) cellIdentifier1 {
@@ -491,25 +387,11 @@ CGFloat wbheight = 0.1f;
                                                                       options:NSLayoutFormatDirectionLeadingToTrailing
                                                                       metrics:nil
                                                                         views:viewsDictionary]];
-    // Fix descriptionText Horizontal.
-//    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-[descriptionText]-|"
-//                                                                      options:NSLayoutFormatDirectionLeadingToTrailing
-//                                                                      metrics:nil
-//                                                                        views:viewsDictionary]];
-    
-    // Fix itemsTable/descriptionText Vertically.
-    //if (self.descriptionText.isHidden) {
-        [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-[answersTable]-|"
-                                                                          options:NSLayoutFormatDirectionLeadingToTrailing
-                                                                          metrics:nil
-                                                                            views:viewsDictionary]];
-//    } else {
-//        [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:[NSString stringWithFormat:@"V:|-[descriptionText(==%f)]-[answersTable]-|",
-//                                                                                   self.descriptionText.bounds.size.height]
-//                                                                          options:NSLayoutFormatDirectionLeadingToTrailing
-//                                                                          metrics:nil
-//                                                                            views:viewsDictionary]];
-//    }
+    // Fix itemsTable Vertically.
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-[answersTable]-|"
+                                                                      options:NSLayoutFormatDirectionLeadingToTrailing
+                                                                      metrics:nil
+                                                                        views:viewsDictionary]];
 }
 
 
@@ -522,9 +404,6 @@ CGFloat wbheight = 0.1f;
     NSString *feedback = @"";
     NSString *title = @"";
 
-//    BOOL incompleteAnswer = FALSE;
-//    BOOL hascorrectAnswer = FALSE;
-    
     NSInteger correct = 0;
     NSInteger wrong = 0;
     NSInteger answered = 0;
